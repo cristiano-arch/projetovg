5ec432b8981bc44a0bb4affe89856887e50f0620
Cristiano Jr <cristianoandre96@gmail.com>
2021-09-12 15:54:28 -0300

    Primeiro commit: Projeto criado

---
+++ src/main/java/com/andcris/projetovg/ProjetovgApplication.java
@@ -0,0 +1,13 @@
+package com.andcris.projetovg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetovgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetovgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andcris/projetovg/ProjetovgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andcris.projetovg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
