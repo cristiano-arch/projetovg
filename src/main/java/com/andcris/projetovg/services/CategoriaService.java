8f07f29aeea0f27e64a89b70a5a0301f53af67d1
Cristiano Jr <cristianoandre96@gmail.com>
2021-09-28 17:08:10 -0300

    Busca de Categoria por id

--- src/main/java/com/andcris/projetovg/services/CategoriaService.java
+++ src/main/java/com/andcris/projetovg/services/CategoriaService.java
@@ -1,12 +1,14 @@
 package com.andcris.projetovg.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andcris.projetovg.domain.Categoria;
 import com.andcris.projetovg.repositories.CategoriaRepository;
+import com.andcris.projetovg.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,5 +18,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
