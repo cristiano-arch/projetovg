7df37e25c85065cc13fc196971a6f9a9b5a7ea96
Cristiano Jr <cristianoandre96@gmail.com>
2021-09-29 15:42:20 -0300

    Inserindo Categoria com POST

--- src/main/java/com/andcris/projetovg/resources/CategoriaResource.java
+++ src/main/java/com/andcris/projetovg/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.andcris.projetovg.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andcris.projetovg.domain.Categoria;
 import com.andcris.projetovg.dto.CategoriaDTO;
@@ -32,5 +36,13 @@ public class CategoriaResource {
 	public ResponseEntity<CategoriaDTO> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(new CategoriaDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDTO(objDto);
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/andcris/projetovg/services/CategoriaService.java
+++ src/main/java/com/andcris/projetovg/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andcris.projetovg.domain.Categoria;
+import com.andcris.projetovg.dto.CategoriaDTO;
 import com.andcris.projetovg.repositories.CategoriaRepository;
 import com.andcris.projetovg.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,14 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
